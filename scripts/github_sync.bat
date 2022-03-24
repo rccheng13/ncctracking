61f1e78c2a4a71154337b7dde1877d4e6e91f7e6
rccheng13 <84091350+rccheng13@users.noreply.github.com>
2022-03-24 14:30:05 -0400

    scheduled push

--- scripts/github_sync.bat
+++ scripts/github_sync.bat
@@ -1,4 +1,8 @@
 cd ncctracking
 "C:\Program Files\Git\cmd\git.exe" add --all
 "C:\Program Files\Git\cmd\git.exe" commit -m "scheduled push"
+<<<<<<< HEAD
 "C:\Program Files\Git\cmd\git.exe" push https://TOKEN@github.com/rccheng13/ncctracking.git main
+=======
+"C:\Program Files\Git\cmd\git.exe" push https://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@github.com/rccheng13/ncctracking.git main
+>>>>>>> 10d23ed (scheduled push)
