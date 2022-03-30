05a0df455ad596f407a0186a3f5fe8fdb7bb8029
rccheng13 <84091350+rccheng13@users.noreply.github.com>
2022-03-30 10:16:31 -0400

    scheduled push

--- scripts/AutoCensus.bat
+++ scripts/AutoCensus.bat
@@ -15,9 +15,5 @@ rem sync to github for graphing
 cd ncctracking
 "C:\Program Files\Git\cmd\git.exe" add --all
 "C:\Program Files\Git\cmd\git.exe" commit -m "scheduled push"
-<<<<<<< HEAD
-"C:\Program Files\Git\cmd\git.exe" push https://TOKEN@github.com/rccheng13/ncctracking.git main
-=======
-"C:\Program Files\Git\cmd\git.exe" push https://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@github.com/rccheng13/ncctracking.git main
->>>>>>> 10d23ed (scheduled push)
+
 rem "C:\Program Files\Git\cmd\git.exe" push origin main
